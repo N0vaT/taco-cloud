30da84dd63d414ea7fe177b2dc96a9799b09de96
N0vaT <Novikovto92@yandex.ru>
2022-08-13 07:00:02 +0300

    1 commit - start project

---
+++ src/main/java/com/nova/tacocloud/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.nova.tacocloud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
