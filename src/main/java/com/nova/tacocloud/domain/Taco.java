00f2fedaab4ca95dabe4cc998b09bde504835566
N0vaT <Novikovto92@yandex.ru>
2022-08-13 18:11:44 +0300

    4 commit - adding validation

--- src/main/java/com/nova/tacocloud/domain/Taco.java
+++ src/main/java/com/nova/tacocloud/domain/Taco.java
@@ -2,12 +2,18 @@ package com.nova.tacocloud.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min = 3, max=20, message = "Поле должно содержать минимум 3 и максимум 20 символов")
     private String name;
+    @NotNull
+    @Size(min = 1, message = "Выберети хотя бы 1 ингридиет")
     private List<Ingredient> ingredients;
 
 }
--- src/main/java/com/nova/tacocloud/domain/TacoOrder.java
+++ src/main/java/com/nova/tacocloud/domain/TacoOrder.java
@@ -1,20 +1,32 @@
 package com.nova.tacocloud.domain;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class TacoOrder {
 
+    @NotBlank(message = "Обязательное поле")
     private String deliveryName;
+    @NotBlank(message = "Обязательное поле")
     private String deliveryStreet;
+    @NotBlank(message = "Обязательное поле")
     private String deliveryCity;
+    @NotBlank(message = "Обязательное поле")
     private String deliveryState;
+    @NotBlank(message = "Обязательное поле")
     private String deliveryZip;
+    @CreditCardNumber(message = "Некорректный номер карты")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "Формат ввода - MM/YY. Пример - 23/24")
     private String ccExpiration;
+    @Digits(integer = 3,fraction = 0, message = "Введите CVV. Пример - 123")
     private String ccCVV;
 
     private List<Taco> tacos = new ArrayList<>();
